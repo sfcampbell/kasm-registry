6ada9a497e57f1c3f95c07a1ed64a717b5a39bf3
SFCampbell <sfcampbell@gmail.com>
2024-01-26 23:00:22 -0700

    first commit

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,8 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'SFCampbell\'s Kasm Workspaces',
+    description: 'Custom Kasm workspaces by SFCampbell.',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
